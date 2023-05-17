f4c7fba2d4049440977fcae27f364e7da4faf42e
Sahilshajahan <96463402+Sahilshajahan@users.noreply.github.com>
2023-05-17 17:30:06 +0530

    Testing Git ignore commands.

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header(props) {
-    return (
-        <div>
-            <h1> Hello i am Header by {props.data} <small><i>in header.js file and this is imported in app.js</i></small></h1>
-        </div>
-    )
-}
-
-export default Header
